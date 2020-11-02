31cefded944d16a03f21fc09a31fee4e86210f68
Kilian Krampf <kilian@krampf.de>
2020-11-02 16:50:04 +0100

    Implementation Module setup

---
+++ template/02_implementation/src/test/java/SimulationTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class SimulationTest {
+    @Test
+    public void testJUnitWorking(){
+        Assertions.assertFalse(false);
+    }
+}
